068b7465a3d7637af1aff44d6150bec61d5d2a84
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2024-10-20 14:51:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as readme:
 
 setuptools.setup(
     name='threerra',
-    version='0.0.1',
+    version='0.0.2',
     author='Jorge Gidi',
     author_email='jorgegidi@gmail.com',
     description='A Qiskit module for three-level systems',
